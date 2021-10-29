68d0908cf142b62c57918521505544997287a361
Саня <@DESKTOP-7VFJ52O>
2021-10-29 20:24:32 +0300

    Add project files.

---
+++ Shop18/ENV.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop18
+{
+    public class ENV
+    {
+        public const string ImagePath = @"\images\products\";
+        public const string SessionCart = "EcBvhuxyH*8AQk_*Mz3LGKcV&fzg778*";
+
+        public static string AdminRole = "Admin";
+        public static string CustomerRole = "Customer";
+    }
+}
---
+++ Shop18/Models/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop18.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string TelegramName { get; set; }
+        public string FacebookURL { get; set; }
+    }
+}
